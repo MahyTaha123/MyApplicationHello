76f8255eea30fc2c4cc32f2b827ce40a8f912a63
Dell <mahy_taha@yahoo.com>
2020-03-23 21:40:29 +0200

    firstcommit

--- app/src/main/java/com/example/myapplicationhello/MainActivity.java
+++ app/src/main/java/com/example/myapplicationhello/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void testUpload() {
+
+        int i = 0;
+        i += 1;
+
+
     }
 }
+
